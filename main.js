7c381b6ef59752e59995963bd332be32d26d78c5
drnieves <ernestonievesurosario@gmail.com>
2021-09-12 22:31:50 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const app = document.getElementById("typewriter");
-const typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
-
-typewriter.typeString("La capital mundial del llanero").pauseFor(200).start();
